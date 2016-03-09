c352bc2ac002a1d3a3438431308b2acfa0eb63d5
Troy Griggs <troy.griggs@gmail.com>
2016-03-09 15:08:38 -0700

    switched to kt fonts and added active states for nav

--- code/site/config.rb
+++ code/site/config.rb
@@ -150,6 +150,7 @@ activate :directory_indexes
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+set :asset_dir, 'assets'
 
 # Build-specific configuration
 configure :build do
